41e32acf6e61f528b9687be272963afd59f02e67
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-06-30 01:13:13 +0200

    Ajout de l'option 'name' dans la commande 'up'
    Ajout d'une nouvelle option `Name` dans la classe `UpOptions` pour spécifier le nom de la pile, avec un texte d'aide associé. Une vérification a été intégrée dans la méthode de la classe `UpCommand` pour afficher le nom de la pile si l'option est fournie, améliorant ainsi la personnalisation lors de l'exécution de la commande 'up'.

--- instancer.cli/Commands/UpCommand.cs
+++ instancer.cli/Commands/UpCommand.cs
@@ -8,6 +8,9 @@ public class UpOptions
     [Option('f', "file", HelpText = "Stack configuration file.")]
     public string? File { get; set; }
 
+    [Option('n', "name", HelpText = "Name of the stack.")]
+    public string? Name { get; set; }
+
     [Option('v', "verbose", HelpText = "Enable verbose output.")]
     public bool Verbose { get; set; }
 }
@@ -20,6 +23,10 @@ public static class UpCommand
         if (!string.IsNullOrEmpty(opts.File))
         {
             Console.WriteLine($"File: {opts.File}");
+        }
+        if (!string.IsNullOrEmpty(opts.Name))
+        {
+            Console.WriteLine($"Name of the stack : {opts.Name}");
         }
         if (opts.Verbose)
         {
